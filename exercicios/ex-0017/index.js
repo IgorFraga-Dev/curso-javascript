e3e2690c4f315c9cb95988845fd0fb10c15262c0
Igor Oliveira Fraga <igorfraga7124@gmail.com>
2026-07-20 10:27:55 -0300

    aprendendo js

---
+++ exercicios/ex-0017/index.js
@@ -0,0 +1,30 @@
+var  c =1 
+do {
+    console.log(` passo ${c}`);
+    c++
+    
+} while ( c <=  3)
+
+/*
+var  c =1 
+while ( c <=  3) {
+console.log(` passo ${c}`);
+c++
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+console.log('Tudo bem ? ');
+*/
